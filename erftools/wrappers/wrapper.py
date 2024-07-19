cc94fecf6eb8bcc7109e2d769c164007f7d35475
Eliot Quon <eliot.quon@nrel.gov>
2024-07-19 13:46:29 -0600

    Generate input_sounding

--- erftools/wrappers/wrapper.py
+++ erftools/wrappers/wrapper.py
@@ -4,6 +4,8 @@ import glob
 import subprocess
 import numpy as np
 
+from ..input_sounding import InputSounding
+
 class Wrapper(object):
     solver = 'EXEC_REL_PATH'
 
@@ -45,6 +47,24 @@ class Wrapper(object):
 
     def init(self,z_profile,u_profile,v_profile,th_profile,qv_profile=None,
              p_surf=1e5,th_surf=None,qv_surf=None):
+        assert len(z_profile) == len(u_profile) == len(v_profile) == len(th_profile)
+        if qv_profile is None:
+            qv_profile = np.zeros_like(z_profile)
+        else:
+            assert len(qv_profile) == len(z_profile)
+        if th_surf is None:
+            th_surf = th_profile[0]
+        if qv_surf is None:
+            qv_surf = qv_profile[0]
+        self.input_sounding = InputSounding(
+            p_surf=p_surf,
+            th_surf=th_surf,
+            qv_surf=qv_surf,
+            z_profile=z_profile,
+            th_profile=th_profile,
+            qv_profile=qv_profile,
+            u_profile=u_profile,
+            v_profile=v_profile)
         self.initialized = True
 
     def setup(self):
@@ -53,16 +73,21 @@ class Wrapper(object):
     def check_inputs(self):
         """This is a template"""
 
+    def create_input_files(self):
+        input_sounding_file = os.path.join(self.rundir,'input_sounding')
+        self.input_sounding.write(input_sounding_file, overwrite=True)
+
     def run(self,rundir='.rundir',ncpu=1):
-        """This is a template"""
-        if not self.iniitalized:
-            print('Need to call init()')
-        if not self.setup_complete:
-            print('Need to call setup()')
+        """
+        This is a template
+        """
+        assert self.initialized, 'Need to call init()'
+        assert self.setup_complete, 'Need to call setup()'
         self.check_inputs()
         # setup run directory
-        os.makedir(rundir, exist_ok=True)
-        self._create_inputs(rundir)
+        os.makedirs(rundir, exist_ok=True)
+        self.rundir = rundir
+        self.create_input_files()
         # call solver
         ncpu = min(ncpu, self.ncpus_avail)
         if self.mpirun != '':
@@ -72,7 +97,9 @@ class Wrapper(object):
         cmd += [self.solver,'inputs']
         with open(os.path.join(rundir,'log.out'),'w') as outfile, \
              open(os.path.join(rundir,'log.err'),'w') as errfile:
-            result = subprocess.run(cmd,stdout=outfile,stderr=errfile)
+            result = subprocess.run(cmd, cwd=rundir,
+                                    stdout=outfile,
+                                    stderr=errfile)
 
     def cleanup(self,rundir=None,realclean=True):
         if rundir is None:
