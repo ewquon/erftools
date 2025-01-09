19aca6a73fe2c00318ef537e93973113a40e556c
Eliot Quon <eliot.quon@nrel.gov>
2025-01-09 13:17:02 -0700

    Move wrf-erf mappings to separate file

--- erftools/wrf/namelist.py
+++ erftools/wrf/namelist.py
@@ -3,6 +3,7 @@ Processing for each namelist within a WRF namelist.input file
 """
 from datetime import datetime
 
+from .namelist_mappings import *
 
 class WRFNamelist(object):
     def __init__(self,nmldict):
@@ -169,56 +170,6 @@ class Domains(WRFNamelist):
         self.max_dz = self.getvar('max_dz',default=1000.)
 
 
-pbl_mapping = {
-    0:  'None',
-    1:  'YSU', # YSU scheme
-    2:  'MYJ', # Mellor-Yamada-Janjic TKE scheme
-    3:  'GFS', # Hybrid EDMF GFS scheme
-    4:  'QNSE', # Eddy-diffusivity Mass Flux, Quasi-Normal Scale Elimination PBL
-    5:  'MYNN', # MYNN 2.5/2.6/3.0 level TKE scheme
-    7:  'ACM2',
-    8:  'BouLac',
-    9:  'UW',
-    10: 'TEMF',
-    11: 'Shin-Hong',
-    12: 'GBM',
-    99: 'MRF',
-}
-sfclay_mapping = {
-    0: 'None',
-    1: 'MOST', # Revised MM5 Monin-Obukhov scheme (Jimenez, renamed in v3.6)
-    2: 'MOST', # Monin-Obukhov (Janjic) scheme
-    5: 'MYNN',
-}
-valid_sfclay = {
-    # for each PBL scheme
-    0:  list(range(100)),
-    1:  [1],
-    2:  [2],
-    5:  [1,2,5,91],
-    6:  [1,2,5,91],
-    7:  [1,7,91],
-    8:  [1,2,91],
-    9:  [1,2,91],
-    11: [1,91],
-    12: [1,91],
-    91: [1,91],
-}
-mp_physics_mapping = {
-    0: 'None',
-    1: 'Kessler',
-    2: 'SAM', # comparable to Lin et al. scheme
-    6: 'SAM', # WRF Single-Moment, 6-class scheme
-    28: 'SAM', # aerosol-aware Thompson scheme
-}
-ra_physics_mapping = {
-    0: 'None',
-    4: 'RRTMGP',
-}
-cu_physics_mapping = {
-    0: 'None',
-}
-
 class Physics(WRFNamelist):
     """&physics namelist"""
 
@@ -260,25 +211,6 @@ class Physics(WRFNamelist):
         cu_idx_list = self.getarrayvar('cu_physics')
         self.cu_physics = [cu_physics_mapping.get(idx,'UNKNOWN') for idx in cu_idx_list]
 
-
-diff_opt_mapping = {
-    1: 'simple',
-    2: 'full',
-}
-km_opt_mapping = {
-    1: 'constant',
-    2: 'Deardorff', # 1.5 order TKE closure (3D)
-    3: 'Smagorinsky', # Smagorinsky first-order closure (3D)
-    4: '2D Smagorinsky', # horizontal Smagorinsky closure (diagnosed from just
-                         #   horizontal deformation); vertical diffusion from
-                         #   PBL scheme
-}
-damp_opt_mapping = {
-    0: 'none',
-    1: 'increased diffusion',
-    2: 'Rayleigh relaxation',
-    3: 'Rayleigh implicit gravity-wave damping',
-}
 
 class Dynamics(WRFNamelist):
     """&dynamics namelist"""
