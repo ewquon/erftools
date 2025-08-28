4a602af7670543126af97b1fde74790a488a3c0a
Eliot Quon <eliot.quon@nrel.gov>
2025-08-28 09:57:38 -0600

    Update deps/versions, add pyproject.toml, warn if no cdsapi installed
    Legacy setup.py removed

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# ERFtools
-A collection of scripts for facilitating the usage of ERF.
+# ERF Tools
+A collection of Python-based modules and scripts for facilitating the usage of ERF.
 
 ## Examples
 
@@ -32,5 +32,6 @@ print(log.ds) # data are stored in an xarray dataset
 Some notes and recommendations:
 
 * An aspirational goal is to contribute code that can be used as in the examples above, with clear, intuitive naming and following PEP-8 style as a set of guidelines rather than gospel.
-* To avoid duplication, model constants are defined in `erf.constants`, which should replicate `ERF/Source/ERF_Constants.H`.
-* In the same vein, equation of state evaluations are defined in `erf.EOS`, which should replicate `ERF/Source/Utils/ERF_EOS.H`. 
+* To avoid duplication, model constants are defined in `erftools.constants`, which should replicate `ERF/Source/ERF_Constants.H`.
+* In the same vein, equation of state evaluations are defined in `erftools.EOS`, which should replicate `ERF/Source/Utils/ERF_EOS.H`.
+* Other utilities for calculating/deriving/diagnosing quantities of interest are in `erftools.utils.*`
--- erftools/preprocessing/__init__.py
+++ erftools/preprocessing/__init__.py
@@ -1,24 +1,27 @@
 from .wrf_inputs import WRFInputDeck
 from .grids import LambertConformalGrid
 
-# ERA5 related funrcions
-from .era5.Download_ERA5Data import Download_ERA5_Data
-from .era5.Download_ERA5Data import Download_ERA5_ForecastData
-from .era5.Plot_1D import plot_1d
-from .era5.ReadERA5DataAndWriteERF_IC import ReadERA5_3DData
+try:
+    import cdsapi
+except ModuleNotFoundError:
+    print("Note: Need to install package 'cdsapi' to work with reanalysis data")
+else:
+    # ERA5 related functions
+    from .era5.Download_ERA5Data import Download_ERA5_Data
+    from .era5.Download_ERA5Data import Download_ERA5_ForecastData
+    from .era5.Plot_1D import plot_1d
+    from .era5.ReadERA5DataAndWriteERF_IC import ReadERA5_3DData
 
-
-# GFS related funrcions
-from .gfs.Download_GFSData import Download_GFS_Data
-from .gfs.Download_GFSData import Download_GFS_ForecastData
-from .gfs.Plot_1D import plot_1d
-from .gfs.ReadGFSDataAndWriteERF_IC import ReadGFS_3DData
-from .gfs.ReadGFSDataAndWriteERF_IC_FourCastNetGFS import ReadGFS_3DData_FourCastNetGFS
-
+    # GFS related functions
+    from .gfs.Download_GFSData import Download_GFS_Data
+    from .gfs.Download_GFSData import Download_GFS_ForecastData
+    from .gfs.Plot_1D import plot_1d
+    from .gfs.ReadGFSDataAndWriteERF_IC import ReadGFS_3DData
+    from .gfs.ReadGFSDataAndWriteERF_IC_FourCastNetGFS import ReadGFS_3DData_FourCastNetGFS
 
 try:
     from herbie import Herbie
 except ModuleNotFoundError:
-    print('Note: Need to install herbie to work with HRRR data')
+    print("Note: Need to install package 'herbie-data' to work with HRRR data")
 else:
     from .hrrr import NativeHRRR, hrrr_projection
